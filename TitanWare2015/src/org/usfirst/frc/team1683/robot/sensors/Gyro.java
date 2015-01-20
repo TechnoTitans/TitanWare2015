c4978870399ff6714902d8fb880c45e482cc3c55
Kirans9977 <kiran.saravanan1@gmail.com>
2015-01-20 18:38:57 -0500

    Gyro Class Added

--- TitanWare2015/src/org/usfirst/frc/team1683/robot/sensors/Gyro.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/sensors/Gyro.java
@@ -1,6 +1,24 @@
 package org.usfirst.frc.team1683.robot.sensors;
 
+public class Gyro extends edu.wpi.first.wpilibj.Gyro implements Sensor {
 
-public class Gyro implements Sensor {
+	private final double SENSITIVITY = 0.009; 
 
+	public Gyro(int channel) {
+		super(channel);
+	    setSensitivity(SENSITIVITY);
+	    reset();
+	}
+	
+	//returns the actual angle in degrees that the robot is currently facing.
+    public double getAngle(){
+    	return getAngle();
+    }
+	
+    @Override
+	public double getRaw() {
+		// TODO Auto-generated method stub
+		return getAngle();
+	}
+    
 }
