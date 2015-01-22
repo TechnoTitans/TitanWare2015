19378b8520e28708c325db5c6e68494362b169e7
PixelatedToast <davidl2381@gmail.com>
2015-01-22 12:25:51 -0500

    Added CoordinateOutOfBoundsError and ScreenPos classes. Updated Vision Class.

---
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/vision/CoordinateOutOfBoundsError.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team1683.robot.vision;
+
+public class CoordinateOutOfBoundsError extends Error {
+	
+	
+	public CoordinateOutOfBoundsError(String message) {
+		super(message);
+	}
+}
--- TitanWare2015/src/org/usfirst/frc/team1683/robot/vision/Vision.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/vision/Vision.java
@@ -1,5 +1,24 @@
 package org.usfirst.frc.team1683.robot.vision;
 
-public class Vision {
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
+/**
+ * 
+ * @author David
+ *
+ */
+public class Vision {
+	
+	private NetworkTable table;
+	
+	public Vision() {
+		table = NetworkTable.getTable("Vision"); // Table to get data from RoboRealm
+	}
+	
+	/**
+	 * @return Array of blobs and their centers
+	 */
+	public ScreenPos[] getBlobCenter() {		
+		return null;
+	}
 }
