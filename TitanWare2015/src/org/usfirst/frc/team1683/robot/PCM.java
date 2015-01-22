dc1d62f18e22fb2faee8bde462a0a30c10d8f278
komals <komal.shri1999@gmail.com>
2015-01-22 17:08:36 -0500

    Added solenoids to teh PCM class

--- TitanWare2015/src/org/usfirst/frc/team1683/robot/PCM.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/PCM.java
@@ -2,4 +2,16 @@ package org.usfirst.frc.team1683.robot;
 
 public class PCM {
 	
+	//Solenoids
+	public static final int SOLENOID_0 = 0;
+	public static final int SOLENOID_1 = 1;
+	public static final int SOLENOID_2 = 2;
+	public static final int SOLENOID_3 = 3;
+	public static final int SOLENOID_4 = 4;
+	public static final int SOLENOID_5 = 5;
+	public static final int SOLENOID_6 = 6;
+	public static final int SOLENOID_7 = 7;
+	
+  
+
 }
