78ce53f6355a2921f144be6effeaa5a70a33fdd1
0816seung <cannot.be.identified@gmail.com>
2015-01-24 10:47:11 -0500

    AirStateMachine Complete(ish... maybe) Old DriverStation Code Added

--- TitanWare2015/src/org/usfirst/frc/team1683/robot/main/DriverStation.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/main/DriverStation.java
@@ -1,7 +1,10 @@
 package org.usfirst.frc.team1683.robot.main;
 
 import org.usfirst.frc.team1683.robot.HWR;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class DriverStation {
 	
@@ -12,4 +15,173 @@ public class DriverStation {
 	public static Joystick leftStick = new Joystick(HWR.LEFT_JOYSTICK);
 	public static Joystick rightStick = new Joystick(HWR.RIGHT_JOYSTICK);
 	public static Joystick auxStick = new Joystick(HWR.AUX_JOYSTICK);
+	
+	public static boolean[][] lasts = new boolean[3][11];
+	
+	/**
+     * Sends the value to SmartDashboard 
+     * @param key Value name
+     * @param val double Input value
+     */
+    public static void sendData(String key, double val){
+        try{
+            SmartDashboard.putNumber(key,val);
+        }
+        catch(IllegalArgumentException e){
+        }
+    }
+    
+    /**
+     * Sends the value to SmartDashboard
+     * @param key Value name 
+     * @param val int Input value
+     */
+    public static void sendData(String key, int val){
+        try{
+            SmartDashboard.putNumber(key, val);
+        }
+        catch(IllegalArgumentException e){
+        }
+    }
+     
+    /**
+     * Sends the value to SmartDashboard
+     * @param key Value name 
+     * @param val String Input value
+     */
+    public static void sendData(String key, String val){
+        try{
+            SmartDashboard.putString(key, val);
+        }
+        catch(IllegalArgumentException e){
+        }
+    }
+    
+    /**
+     * Sends the value to SmartDashboard
+     * @param key Value name 
+     * @param val boolean Input value
+     */
+    public static void sendData(String key, boolean val){
+        try{
+            SmartDashboard.putBoolean(key, val);
+        }
+        catch(IllegalArgumentException e){
+        }
+    }
+    
+    /*
+     * Methods to send data to store in Preferences 
+     */
+    
+    /**
+     * Sends the integer to be stored in Preferences in the cRIO
+     * @param key Name of the value 
+     * @param val int Value to be sent 
+     */
+    public static void prefInt(String key, int val){
+        Preferences.getInstance().putInt(key, val);
+    }
+    
+    /**
+     * Sends the boolean to be stored in Preferences in the cRIO
+     * @param key Name of the value 
+     * @param val boolean Value to be sent 
+     */
+    public static void prefBoolean(String key, boolean val){
+        Preferences.getInstance().putBoolean(key, val);
+    }
+    
+    /**
+     * Sends the String to be stored in Preferences in the cRIO
+     * @param key Name of the value
+     * @param val String Value to be sent 
+     */
+    public static void prefString(String key, String val){
+        Preferences.getInstance().putString(key, val);
+    }
+    
+    /**
+     * Sends the double to be stored in Preferences in the cRIO
+     * @param key Name of the value
+     * @param val double value to be sent
+     */
+    public static void prefDouble(String key, double val){
+        Preferences.getInstance().putDouble(key, val);
+    }
+    
+    /*
+     * Methods to get data from the cRIO
+     */
+    
+     /**
+     * Receives the value from cRIO
+     * Default is false if no boolean value is found with the key
+     * @param key Value name 
+     */
+    public static boolean getBoolean(String key){
+        return Preferences.getInstance().getBoolean(key, false);
+    }
+    
+    /**
+     * Receives the value from cRIO
+     * Default is "null" if no string is found with the key
+     * @param key Value name 
+     * @return null if no string is found
+     */
+    public static String getString(String key){
+        return Preferences.getInstance().getString(key, null);
+    }
+    
+    /**
+     * Receives the value from cRIO
+     * Default is 0.0 if no double is found with the key
+     * @param key Value name 
+     * @return 0.0 if key is not found
+     */
+    public static double getDouble(String key){
+        return Preferences.getInstance().getDouble(key, 0.0);
+    }
+    
+    /**
+     * Receives the value from cRIO
+     * Default is 0 if no integer found with the key
+     * @param key Value name 
+     * @return 0 if key is not found 
+     */
+    public static int getInt(String key){
+       return Preferences.getInstance().getInt(key, 0);
+    }
+    
+    /**
+     * This method prevents multiple readings of the joystick
+     * Essentially allowing a button to be held down, and the code will
+     * only read one input
+     *
+     * @param joystick the number that indicates the joystick
+     * @param button the number of the button
+     * @return  It returns a boolean that the code will use
+     */
+    public static boolean antiBounce(int joystick, int button){
+       boolean pressed = false;
+       if (joystick == 1)
+           pressed = DriverStation.leftStick.getRawButton(button);
+       else if (joystick == 2)
+           pressed = DriverStation.rightStick.getRawButton(button);
+       else if (joystick == 3)
+           pressed = DriverStation.auxStick.getRawButton(button);
+       
+       if(pressed && !lasts[joystick-1][button-1]){
+           lasts[joystick-1][button-1] = true;
+           return true;
+       }
+       else if (pressed && lasts[joystick-1][button-1]){
+           return false;
+       }
+       else
+       {
+           lasts[joystick-1][button-1] = false;
+           return false;
+       }
+    }
 }
---
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/test/AirStateMachine.java
@@ -0,0 +1,89 @@
+package org.usfirst.frc.team1683.robot.test;
+
+import org.usfirst.frc.team1683.robot.HWP;
+import org.usfirst.frc.team1683.robot.main.DriverStation;
+import org.usfirst.frc.team1683.robot.pneumatics.AirSystem;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+
+public class AirStateMachine {
+	AirSystem air;
+	Joystick stick;
+	int buttonAssignment;
+	int stickAssignment;
+	double valveDelay;
+	Timer timer;
+	int stickNum;
+	
+	//State Machine Variables
+	static final int INIT_CASE				 = 0;
+	static final int WAIT_FOR_BUTTON		 = 1;
+	static final int OPEN_VALVE				 = 2;
+	static final int DELAY					 = 3;
+	static final int CLOSE_VALVE			 = 4;
+	int presentState = INIT_CASE;
+	int nextState;
+	
+	public AirStateMachine(int[] solenoids, int button) {
+		air = new AirSystem(new Compressor(),solenoids);
+		stick = new Joystick(HWP.JOY_0);
+		buttonAssignment = button;
+		air.retract();
+		timer = new Timer();
+		valveDelay = DriverStation.getDouble("delay");
+		
+		DriverStation.prefDouble("delay", .020);
+	}
+	
+	public AirStateMachine(int[] solenoids, int stickNum, int button) {
+		air = new AirSystem(new Compressor(),solenoids);
+		stick = new Joystick(stickNum);
+		buttonAssignment = button;
+		air.retract();
+		valveDelay = DriverStation.getDouble("delay");
+		this.stickNum = stickNum;
+	}
+	
+	public void run(){
+		switch(presentState){
+		case INIT_CASE:
+		{
+			timer.start();
+			nextState = WAIT_FOR_BUTTON;
+    	}
+		case WAIT_FOR_BUTTON:
+		{
+			if (DriverStation.antiBounce(stickNum, buttonAssignment))
+				nextState = OPEN_VALVE;
+			else
+				nextState = WAIT_FOR_BUTTON;
+			
+		}
+		case OPEN_VALVE:
+		{
+			air.extend();
+			nextState = DELAY;
+		}
+		case DELAY:
+		{
+			if (timer.get()>=valveDelay)
+				nextState = CLOSE_VALVE;
+			else
+				nextState = DELAY;
+		}
+		case CLOSE_VALVE:
+		{
+			timer.reset();
+			air.retract();
+			nextState = WAIT_FOR_BUTTON;
+		}
+		default:
+		{
+			nextState = INIT_CASE;
+		}
+    	presentState = nextState;
+		}
+	}
+}
