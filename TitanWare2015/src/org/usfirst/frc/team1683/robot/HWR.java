f05e321f1232a95c8cca57b7141c672ddd5f66a4
PixelatedToast <davidl2381@gmail.com>
2015-01-30 17:11:50 -0500

    Actually fixed Gyro.

--- TitanWare2015/src/org/usfirst/frc/team1683/robot/HWR.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/HWR.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team1683.robot;
 
 public class HWR {
 	
-	//Joysticks
+	// Joysticks
 	public static final int AUX_JOYSTICK 			= HWP.JOY_0;
 	public static final int LEFT_JOYSTICK 			= HWP.JOY_1;
 	public static final int RIGHT_JOYSTICK 			= HWP.JOY_2;
@@ -10,7 +10,8 @@ public class HWR {
 	public static final int LEFT_MOTOR				= HWP.PWM_0;
 	public static final int RIGHT_MOTOR				= HWP.PWM_1;
 	
-	public static final int GYRO					= HWP.ANALOG_3;
+	// Gyro MUST BE 0 OR 1
+	public static final int GYRO					= HWP.ANALOG_1;
 	
 	public static final int LEFT_CHANNEL_A			= HWP.DIO_1;
 	public static final int LEFT_CHANNEL_B			= HWP.DIO_2;
