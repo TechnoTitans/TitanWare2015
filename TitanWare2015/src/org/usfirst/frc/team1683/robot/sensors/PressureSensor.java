aee56adaa25e87d63984cdda4b70a96d790e0c2d
komals <komal.shri1999@gmail.com>
2015-01-29 19:10:16 -0500

    Updated PressureSensor class.

--- TitanWare2015/src/org/usfirst/frc/team1683/robot/sensors/PressureSensor.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/sensors/PressureSensor.java
@@ -1,13 +1,23 @@
 package org.usfirst.frc.team1683.robot.sensors;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**Class to handle any pressure sensors used.
  * @author Sreyas Mirthipati
  *
  */
 public class PressureSensor implements Sensor {
 	//Instance data
+	private final double MIN_PRESSURE_VOLTS = 0.5;
+	private final double MAX_PRESSURE_VOLTS = 4.5;
+	private final double VOLTAGE_RANGE = MAX_PRESSURE_VOLTS - MIN_PRESSURE_VOLTS;
+	private final double MAX_PRESSURE = 200;
+	private final double PRESSURE_SLOPE = MAX_PRESSURE/VOLTAGE_RANGE;
+	private double averageVoltage;
 	private double pressure;
 	private int portNumber;
+	AnalogInput sensor;
 	
 	/**Constructor
 	 * @param portNumber
@@ -15,15 +25,40 @@ public class PressureSensor implements Sensor {
 	public PressureSensor(int portNumber)
 	{
 		this.portNumber = portNumber;
+		sensor = new AnalogInput(portNumber);
 	}
-
 	
 	/**
      * Gets raw data from pressure sensor
      * @return raw data as double
      */
 	public double getRaw() {
-		return 0;
+		return sensor.getAverageVoltage();
+	}
+	
+	public int getPort() {
+		return portNumber;
+	}
+	
+	public double getPressure() {
+		averageVoltage = sensor.getAverageVoltage();
+        if (averageVoltage < MIN_PRESSURE_VOLTS)
+        {
+            SmartDashboard.putBoolean("LowPressureTransducerError", true);
+            pressure = 0;
+            
+        }
+        else if (averageVoltage > MAX_PRESSURE_VOLTS)
+        {
+            SmartDashboard.putBoolean("LowPressureTransducerError", true);
+            pressure = MAX_PRESSURE;
+        }
+        else
+        {
+            SmartDashboard.putBoolean("LowPressureTransducerError", false);
+            pressure = PRESSURE_SLOPE*(averageVoltage-MIN_PRESSURE_VOLTS);
+        }
+        return pressure;
 	}
 	
 	
