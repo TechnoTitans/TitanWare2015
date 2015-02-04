7d2668df7a49d985ec496626f2b46dff7adaaea0
Sneha007 <kadiyalasneha007@gmail.com>
2015-02-03 19:37:55 -0500

    Added lift pistons

--- TitanWare2015/src/org/usfirst/frc/team1683/robot/pickerupper/PickerUpper.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/pickerupper/PickerUpper.java
@@ -4,36 +4,71 @@ import org.usfirst.frc.team1683.robot.drivetrain.Encoder;
 import org.usfirst.frc.team1683.robot.drivetrain.Motor;
 import org.usfirst.frc.team1683.robot.drivetrain.MotorGroup;
 import org.usfirst.frc.team1683.robot.main.DriverStation;
+import org.usfirst.frc.team1683.robot.pneumatics.AirSystem;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Talon;
 
 
 public class PickerUpper {
 	MotorGroup motors;
-	/**
-	 * Constructor
-	 * @param pickerUpperChannels
-	 * @param motorType
-	 * @param inverseDirection
-	 */
-	public PickerUpper(int[] pickerUpperChannels, Class motorType, boolean inverseDirection){
-		this.motors = new MotorGroup(pickerUpperChannels, motorType, inverseDirection);
-	}
+	AirSystem liftPistons;
+	MotorGroup liftMotors;
+	
 	/**
 	 * Constructor
 	 * @param pickerUpperChannels
 	 * @param talonSRX
 	 * @param inverseDirection
-	 * @param beltChannelA
-	 * @param beltChannelB
 	 */
-	public PickerUpper(int[] pickerUpperChannels, Class motorType, boolean inverseDirection,
-			int beltChannelA, int beltChannelB){
-		this.motors = new MotorGroup(pickerUpperChannels, motorType, inverseDirection, 
-				new Encoder(beltChannelA, beltChannelB, inverseDirection));
+	public PickerUpper(int[] pickerUpperChannels, Class<Motor> talonSRX, boolean inverseDirection){
+		this.motors = new MotorGroup(pickerUpperChannels, talonSRX, inverseDirection);
+		
 	}
+/**
+ * Constructor
+ * @param pickerUpperChannels
+ * @param talonSRX
+ * @param inverseDirection
+ * @param beltChannelA
+ * @param beltChannelB
+ * @param rightPiston
+ * @param leftPiston
+ * @param leftMotor
+ * @param rightMotor
+ * @param motorType
+ */
+	public PickerUpper(int[] pickerUpperChannels, Class<Motor> talonSRX, boolean inverseDirection,
+			int beltChannelA, int beltChannelB, int rightPiston, int leftPiston, int leftMotor, int rightMotor, Class<Motor> motorType){
+		this.motors = new MotorGroup(pickerUpperChannels, talonSRX, inverseDirection, 
+				new Encoder(beltChannelA, beltChannelB, inverseDirection));
+		int[] channelNumbers = {leftMotor, rightMotor};
+		int[] pistons = {rightPiston, leftPiston};
+		liftPistons = new AirSystem(new Compressor(), pistons);
+		liftMotors = new MotorGroup(channelNumbers, motorType , false);
+	}
+	/**
+	 * Lifts the pickerupper device into the straight position
+	 */
+	public void uprightPickerUpper() {
+		liftPistons.extend();
+	}
+	
+	/**
+	 * Brings back the pickerupper device into an angle
+	 */
+	public void angledPickerUpper() {
+		liftPistons.retract();
+	}
+	
+	
 	/**
 	 * runs the motors for the pickerupper
 	 */
 	public void run(){
 		motors.set(DriverStation.auxStick.getRawAxis(DriverStation.YAxis));
 	}
+	
+	
 }
