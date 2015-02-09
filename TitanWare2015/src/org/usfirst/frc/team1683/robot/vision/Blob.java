669a0578b0317e01708fb478aa3432cd0daf8133
PixelatedToast <davidl2381+github@gmail.com>
2015-02-08 21:45:37 -0500

    Added alternative getClosestBlob2 method in Vision for getting the closest blob from area. Modified Blob class to accomodate for getClosestBlob2. Finished (hopefully) centerClosestBlob method.

--- TitanWare2015/src/org/usfirst/frc/team1683/robot/vision/Blob.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/vision/Blob.java
@@ -7,10 +7,11 @@ package org.usfirst.frc.team1683.robot.vision;
 public class Blob {
 	
 	public final int INDEX;
-	public final int HEIGHT;
-	public final int WIDTH;
-	public final int X_POS;
-	public final int Y_POS;
+	public final double HEIGHT;
+	public final double WIDTH;
+	public final double X_POS;
+	public final double Y_POS;
+	public final double AREA;
 	
 	/**
 	 * Constructor
@@ -20,12 +21,13 @@ public class Blob {
 	 * @param X_POS		X position of center of blob
 	 * @param Y_POS		Y position of center of blob
 	 */
-	public Blob(int INDEX, int HEIGHT, int WIDTH, int X_POS, int Y_POS) {
+	public Blob(int INDEX, double HEIGHT, double WIDTH, double X_POS, double Y_POS, double AREA) {
 		this.INDEX 	= INDEX;
 		this.HEIGHT = HEIGHT;
 		this.WIDTH  = WIDTH;
 		this.X_POS 	= X_POS;
 		this.Y_POS 	= Y_POS;
+		this.AREA 	= AREA;
 	}
 	
 	/**
--- TitanWare2015/src/org/usfirst/frc/team1683/robot/vision/Vision.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/vision/Vision.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1683.robot.vision;
 
-import edu.wpi.first.wpilibj.CameraServer;
+import org.usfirst.frc.team1683.robot.drivetrain.TankDrive;
+
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.networktables2.type.NumberArray;
 import edu.wpi.first.wpilibj.tables.TableKeyNotDefinedException;
@@ -17,48 +18,53 @@ public class Vision {
 	private NumberArray COG_Y;
 	private NumberArray HEIGHT; // Height of blobs in Pixels
 	private NumberArray WIDTH; // Width of blobs in Pixels
+	private NumberArray AREA; // Area of blobs in Pixels
 	private int blobCount;
 	
 	// Camera ID manually retrieved from RoboRIO web interface.
-	private final String CAM_ID 			= "cam1";
+//	private final String CAM_ID 			= "cam1";
 	
 	// Predetermined actual dimensions of object represented by blob.
 	// All variables are in Meters.
-	private final double REF_HEIGHT 		= 19.5*2.54/100; 
+//	private final double REF_HEIGHT 		= 19.5*2.54/100; 
 	private final double REF_WIDTH 			= 29.25*2.54/100;
 	private final double TOTE_WIDTH 		= 26.9*2.54/100;
-	private final double TOTE_HEIGHT 		= 12.1*2.54/100;
+//	private final double TOTE_HEIGHT 		= 12.1*2.54/100;
 	private final double REF_DISTANCE		= 21*2.54/100; // Always in meters.
 	// Resolution of picture.
 	private final int REF_HEIGHT_PIXELS 	= 240;
 	private final int REF_WIDTH_PIXELS		= 320;
 	// Calculated Field of View
-	private final double HORIZ_FOV_DEG		= 67; //69.7089;
-	private final double VERT_FOV_DEG		= 49.8095;
+//	private final double HORIZ_FOV_DEG		= 67; //69.7089;
+//	private final double VERT_FOV_DEG		= 49.8095;
 	
 	private final double CENTER_X = REF_HEIGHT_PIXELS/2;
 	private final double CENTER_Y = REF_WIDTH_PIXELS/2;
 	
-	
 	private final double FOCAL_LENGTH		= (REF_WIDTH_PIXELS*REF_DISTANCE)/REF_WIDTH;
+	
+	// Distance away from center needed in order to be considered centered.
+	private final double CENTER_THRESHOLD	= 10.0;
 	
 	/**
 	 * Constructor
 	 * Starts camera server for USB camera
 	 */
 	public Vision() {
-		CameraServer.getInstance().startAutomaticCapture(CAM_ID);
+//		CameraServer.getInstance().startAutomaticCapture(CAM_ID);
 		table 		= NetworkTable.getTable("Vision"); // Table to get data from RoboRealm
 		COG_X		= new NumberArray();
 		COG_Y 		= new NumberArray();
 		HEIGHT		= new NumberArray();
 		WIDTH		= new NumberArray();
+		AREA		= new NumberArray();
 	} 
 	
 	/**
 	 * @return Returns all the blobs seen on screen and their properties in an array.
 	 */
 	public Blob[] getData() {
+		// Should not ever be null.
 		blobCount 	= (int) Vision.table.getNumber("BLOB_COUNT");
 		Blob[] blobs = new Blob[blobCount];
 		
@@ -67,14 +73,15 @@ public class Vision {
 			Vision.table.retrieveValue("COG_X", 	COG_X);
 			Vision.table.retrieveValue("COG_Y", 	COG_Y);
 			Vision.table.retrieveValue("HEIGHT", 	HEIGHT);
-			Vision.table.retrieveValue("WIDTH", 	WIDTH);			
+			Vision.table.retrieveValue("WIDTH", 	WIDTH);
+			Vision.table.retrieveValue("AREA", 		AREA);
 		}
 		catch(TableKeyNotDefinedException exp) {
 			System.out.println("TableKeyNotDefinedException");
 		}
 		
 		for (int i = 0; i < blobs.length; i++) {
-			blobs[i] = new Blob(i, (int) HEIGHT.get(i), (int) WIDTH.get(i), (int) COG_X.get(i), (int) COG_Y.get(i));
+			blobs[i] = new Blob(i, HEIGHT.get(i), WIDTH.get(i), COG_X.get(i), COG_Y.get(i), AREA.get(i));
 		}
 		
 		return blobs;
@@ -105,6 +112,7 @@ public class Vision {
 	 */
 	public Blob getClosestBlob(Blob[] blobs) {
 		Blob closest = blobs[0];
+		// Successful use of for each loop.
 		for (Blob blob : blobs) {
 			if (calcDistance(blob) < calcDistance(closest)) {
 				closest = blob;
@@ -112,19 +120,55 @@ public class Vision {
 		}
 		return closest;
 	}
+	
 	/**
-	 * Gets the vector pointing towards the center.
-	 * @return Vector object representing center vector;
+	 * Another implementation of getting the closest Blob in an array.
+	 * Uses area of blob instead of calculated distance.
+	 * May or may not be more accurate.
+	 * @param blobs Array of blobs.
+	 * @return The closest Blob
 	 */
-	public Vector getCenterVector(Blob blob) {
-		Vector center = new Vector(blob.X_POS, blob.Y_POS, CENTER_X, CENTER_Y);
-		return center;
+	public Blob getClosestBlob2(Blob[] blobs) {
+		Blob closest = blobs[0];
+		// Successful use of for each loop.
+		for (Blob blob : blobs) {
+			if (blob.AREA < closest.AREA) {
+				closest = blob;
+			}
+		}
+		return closest;
 	}
+	
+//	/**
+//	 * Gets the vector pointing towards the center.
+//	 * @return Vector object representing center vector;
+//	 */
+//	public Vector getCenterVector(Blob blob) {
+//		Vector center = new Vector(blob.X_POS, blob.Y_POS, CENTER_X, CENTER_Y);
+//		return center;
+//	}
 	
 	/**
 	 * Attempts to center the closest blob on screen horizontally.
 	 */
-	public void centerClosestBlob() {
+	public void centerClosestBlob(TankDrive driver) {
+		Blob[] data = getData();
 		
+		final double SPEED = 3;
+		
+		if (Math.abs(getClosestBlob(data).X_POS - CENTER_X) <= CENTER_THRESHOLD) {
+			driver.stop(); // No idea how this method is supposed to work yet.
+		}
+		// If right of tote, go left.
+		else if (getClosestBlob(data).X_POS - CENTER_X > 0) {
+			driver.goSideways(-SPEED);
+		}
+		// If left of tote, go right.
+		else if(getClosestBlob(data).X_POS - CENTER_X < 0) {
+			driver.goSideways(SPEED);
+		}
+		else {
+			driver.stop();
+		}
 	}
 }
