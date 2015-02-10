7d4bd121562b4acb3145ff2098cc2481fd65421d
Sreyas Mirthipati <sreyas.mirthipati@gmail.com>
2015-02-10 18:27:18 -0500

    Finished Photogate class

--- TitanWare2015/src/org/usfirst/frc/team1683/robot/sensors/Photogate.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/sensors/Photogate.java
@@ -1,8 +1,31 @@
 package org.usfirst.frc.team1683.robot.sensors;
 
-public class Photogate implements Sensor {
+/**This class handles photogate sensors.
+ * @author Sreyas Mirthipati
+ *
+ */
+public class Photogate extends edu.wpi.first.wpilibj.DigitalInput implements Sensor {
 
+	boolean isBlocked;
 	
+	/**Constructor
+	 * @param portNumber - the DIO port that the sensor is connected to
+	 */
+	public Photogate(int portNumber){
+		super(portNumber);
+	}
+
+	/**
+	 * @return a boolean that is true if the photogate is blocked
+	 */
+	public boolean getIsBlocked() {
+		return super.get();
+	}
+
+	/**DO NOT USE THIS METHOD
+	 * 
+	 * @see org.usfirst.frc.team1683.robot.sensors.Sensor#getRaw()
+	 */
 	@Override
 	public double getRaw() {
 		return 0;
