70518a3468773c795bd2436a0aa14ab4946e7d62
Sreyas Mirthipati <sreyas.mirthipati@gmail.com>
2015-02-09 19:06:15 -0500

    Added tilt status enum test to BuiltInAccelTester class

--- TitanWare2015/src/org/usfirst/frc/team1683/robot/test/BuiltInAccelTester.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/test/BuiltInAccelTester.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1683.robot.test;
 
 import org.usfirst.frc.team1683.robot.main.DriverStation;
 import org.usfirst.frc.team1683.robot.sensors.BuiltInAccel;
+import org.usfirst.frc.team1683.robot.sensors.BuiltInAccel.TiltStatus;
 
 /**This class contains tests for the BuiltInAccel class
  * @author Sreyas Mirthipati
@@ -16,12 +17,27 @@ public class BuiltInAccelTester implements Tester {
 	}
 	@Override
 	public void test() {
+		String status;
 		DriverStation.sendData("Raw", accel.getRaw());
 		DriverStation.sendData("X", accel.getX());
 		DriverStation.sendData("Y", accel.getY());
 		DriverStation.sendData("Z", accel.getZ());
 		DriverStation.sendData("YZ Angle", accel.getYZAngle());
 		DriverStation.sendData("XZ Angle", accel.getXZAngle());
+		if (accel.getStatus().equals(TiltStatus.FLAT))
+			status = "FLAT";
+		else if(accel.getStatus().equals(TiltStatus.CLIMBING))
+			status = "CLIMBING";
+		else if(accel.getStatus().equals(TiltStatus.DESCENDING))
+			status = "DESCENDING";
+		else if(accel.getStatus().equals(TiltStatus.TIPPING))
+			status = "TIPPING";
+		else if(accel.getStatus().equals(TiltStatus.UNDETERMINED))
+			status = "UNDETERMINED";
+		else
+			status = "ERROR";
+		DriverStation.sendData("Tilt Status", status);
+			
 	}
 
 }
