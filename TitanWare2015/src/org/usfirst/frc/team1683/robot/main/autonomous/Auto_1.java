fc73728b45e88c28cfe5b8471de9da9011629574
0816seung <cannot.be.identified@gmail.com>
2015-02-16 16:02:27 -0500

    Non-thread based Auto_1

--- TitanWare2015/src/org/usfirst/frc/team1683/robot/main/autonomous/Auto_1.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/main/autonomous/Auto_1.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team1683.robot.main.autonomous;
 
+import org.usfirst.frc.team1683.robot.HWR;
 import org.usfirst.frc.team1683.robot.drivetrain.HDrive;
 import org.usfirst.frc.team1683.robot.pickerupper.PickerUpper;
 import org.usfirst.frc.team1683.robot.vision.Vision;
@@ -17,15 +18,36 @@ public class Auto_1 extends Autonomous{
 		case INIT_CASE:
 		{
 			timer.start();
+			nextState = State.START_DRIVE_FORWARD;
+			break;
+		}
+		case START_DRIVE_FORWARD:
+		{
+			hDrive.leftEncoder.reset();
+			hDrive.rightEncoder.reset();
 			nextState = State.DRIVE_FORWARD;
 			break;
 		}
 		case DRIVE_FORWARD:
 		{
-			System.out.println("driveDistance = " + driveDistance);
-//			hDrive.goForward(driveDistance);
-			hDrive.goForward(143);
-			nextState = State.END_CASE;
+			double speed;
+			if (driveDistance>0){
+				speed = HWR.MEDIUM_SPEED;
+			}
+			else{
+				speed = -HWR.MEDIUM_SPEED;
+			}
+			if (Math.abs(hDrive.leftEncoder.getDisplacement(hDrive.leftEncoder.getDistancePerPulse()))<Math.abs(driveDistance)&&
+					Math.abs(hDrive.rightEncoder.getDisplacement(hDrive.rightEncoder.getDistancePerPulse()))<Math.abs(driveDistance))
+			{
+				hDrive.setTankDrive(speed);
+				nextState = State.DRIVE_FORWARD;
+			}
+			else
+			{
+				hDrive.stop();
+				nextState = State.END_CASE;
+			}
 			break;
 		}
 		case END_CASE:
