7a126273672f1fed72ac541a15d48d15b271ba81
komals <komal.shri1999@gmail.com>
2015-03-05 19:22:11 -0500

    Added Auto_7

---
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/main/autonomous/Auto_7.java
@@ -0,0 +1,72 @@
+package org.usfirst.frc.team1683.robot.main.autonomous;
+
+import org.usfirst.frc.team1683.robot.drivetrain.HDrive;
+import org.usfirst.frc.team1683.robot.main.autonomous.Autonomous.State;
+import org.usfirst.frc.team1683.robot.pickerupper.PickerUpper;
+import org.usfirst.frc.team1683.robot.vision.Vision;
+
+public class Auto_7 extends Autonomous {
+	public Auto_7 (HDrive drive, PickerUpper pickerUpper, Vision vision){
+		super(drive, pickerUpper, vision);
+	}
+
+	public void run(){
+		switch(presentState)
+		{
+		case INIT_CASE:
+		{
+			delay();
+			timer.start();
+			nextState = State.LIFT_TOTE;
+			break;
+		}
+		case LIFT_TOTE:
+		{
+			pickerUpper.liftFirstTote();
+			waitForThread(pickerUpper.getCurrentThread());
+			nextState = State.DRIVE_FORWARD;
+			break;
+		}
+		case DRIVE_FORWARD:
+		{
+			hDrive.resetTankEncoders();
+			hDrive.goForward(driveDistance);
+			waitForThread(hDrive.left.getCurrentThread(), hDrive.right.getCurrentThread());
+			nextState = State.DROP_TOTE;
+			break;
+		}
+		case DROP_TOTE:
+		{
+			pickerUpper.drop();
+			waitForThread(pickerUpper.getCurrentThread());
+			nextState = State.DRIVE_BACKWARD;
+			break;
+		}
+		case DRIVE_BACKWARD:
+		{
+			hDrive.goForward(-backDistance);
+			waitForThread(hDrive.left.getCurrentThread(), hDrive.right.getCurrentThread());
+			nextState = State.END_CASE;
+			break;
+		}
+		case END_CASE:
+		{
+			hDrive.stop();
+			nextState = State.END_CASE;
+			break;
+		}
+		default:
+		{
+			defaultState();
+			nextState = State.END_CASE;
+			break;
+		}
+		}
+		printState();
+		presentState = nextState;
+	}
+
+
+
+
+}
