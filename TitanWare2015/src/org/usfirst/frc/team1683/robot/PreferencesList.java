c10206ff5934c1bcc01354fe23a995e1fa9012c1
Sneha007 <kadiyalasneha007@gmail.com>
2015-03-07 14:17:34 -0500

    added to Preferences List

--- TitanWare2015/src/org/usfirst/frc/team1683/robot/PreferencesList.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/PreferencesList.java
@@ -21,6 +21,8 @@ public class PreferencesList{
 		DriverStation.prefBoolean("enablePrinting", true);
 		DriverStation.prefDouble("driveTime", 1.75); //sec
 		DriverStation.prefDouble("sideTime", 2); //sec
+		DriverStation.prefDouble("coopDistance",123);
+		DriverStation.prefDouble("stepDistance", 12.3);
 		
 		DriverStation.prefBoolean("enableLightSensor", true);
 		
