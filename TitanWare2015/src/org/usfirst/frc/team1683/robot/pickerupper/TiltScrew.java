b00399fe860c9c9922ea11ef1d2a2c123a5262ee
Animesh Koratana <animesh.koratana@gmail.com>
2015-03-24 17:07:48 -0400

    Threaded Version of TiltScrew

--- TitanWare2015/src/org/usfirst/frc/team1683/robot/pickerupper/TiltScrew.java
+++ TitanWare2015/src/org/usfirst/frc/team1683/robot/pickerupper/TiltScrew.java
@@ -4,41 +4,40 @@ import org.usfirst.frc.team1683.robot.HWR;
 import org.usfirst.frc.team1683.robot.drivetrain.MotorGroup;
 
 
-public class TiltScrew {
+public class TiltScrew implements Runnable{
 	private MotorGroup tiltMotor;
 	private PickupState state;
+	private PickupState targetState;
 	protected static double speed = HWR.TILTSCREW_SPEED;
+	Thread currentThread; 
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public TiltScrew(int motorPort, Class motorType, boolean inverseDirection) {
 		tiltMotor = new MotorGroup("TiltMotor", new int[]{motorPort}, motorType, inverseDirection);
 		tiltMotor.enableBrakeMode(true);
 		tiltMotor.enableLimitSwitch(true, true);
+		targetState = null;
 	}
 
 	/**
 	 * Drives the motor forwards until front limit switch is closed
 	 */
 	public void tiltUpright() {
-		if(!tiltMotor.getMotor(0).isFwdLimitSwitchClosed()) {
-			tiltMotor.set(-speed);
-		}
-		else {
-			tiltMotor.stop();
-		}
+		targetState = PickupState.VERTICAL;
+		currentThread = new Thread(this, "Upright Picker Upper");
+		currentThread.start();
 	}
 
 	/**
 	 * Drives the motor backwards until rear limit switch is closed
 	 */
 	public void tiltBackward() {
-		if(!tiltMotor.getMotor(0).isRevLimitSwitchClosed()) {
-			tiltMotor.set(speed);
-		}
-		else {
-			tiltMotor.stop();
-		}
+		targetState = PickupState.ANGLED;
+		currentThread = new Thread(this, "Backward Picker Upper");
+		currentThread.start();
 	}
+	
+	
 
 	/**
 	 * @return the tilt MotorGroup
@@ -47,12 +46,30 @@ public class TiltScrew {
 		return tiltMotor;
 	}
 	
+	public Thread getCurrentThread(){
+		return currentThread;
+	}
+	
 	public void setState(PickupState state) {
 		this.state = state;
 	}
 	
 	public PickupState getState() {
 		return state;
+	}
+
+	@Override
+	public void run() {
+		while (targetState != state){
+			if (targetState == PickupState.ANGLED){
+				tiltMotor.set(speed);
+			}else if (targetState == PickupState.VERTICAL){
+				tiltMotor.set(-speed);
+			}
+		}
+		tiltMotor.stop();
+		currentThread.destroy();
+		currentThread.notifyAll();
 	}
 	
 }
